dacce5dc6e6d6518414d464ee19646ff4b82c1cd
Sophie Wong <83475667+panda667@users.noreply.github.com>
2025-04-08 15:27:37 -0700

    Add vigenere capability to Cipher.py

--- Cryptography/Cipher.py
+++ Cryptography/Cipher.py
@@ -68,22 +68,22 @@ class Cipher:
                 output=output+chr(num)
             else: output=output+" "
         return output
-    c_map={"A":"H","B":"L","C":"B","D":"J",
+    map1={"A":"H","B":"L","C":"B","D":"J",
            "E":"D","F":"Q","G":"S","H":"A",
            "I":"P","J":"Y","K":"G","L":"R",
            "M":"X","N":"K","O":"M","P":"V",
            "Q":"C","R":"I","S":"T","T":"F",
            "U":"N","V":"Z","W":"U","X":"W",
            "Y":"E","Z":"O"," ":" "}
-    reverse_map={v:k for k,v in c_map.items()}
+    reverse_map1={v:k for k,v in map1.items()}
     def map_cipher(s,direction):
         s=s.upper()
         output=""
         for ch in s:
-            if direction=="encrypt":
-                output=output+Cipher.c_map[ch]
-            elif direction=="decrypt":
-                output=output+Cipher.reverse_map[ch]
+            if direction=="e":
+                output=output+Cipher.map1[ch]
+            elif direction=="d":
+                output=output+Cipher.reverse_map1[ch]
         return output
     def rot_more(s):
         output=""
@@ -97,9 +97,18 @@ class Cipher:
                 pass
             else: output=output+chr(10)
         return output
-    def encrypt_xor(s,key):
+    def vigenere(s,k,f):
         output=""
-        for ch in s:
-            num=ord(ch)^key
-            output=output+chr(num)
+        s=s.upper()
+        s="".join(s.split())
+        k=k.upper()
+        k_rep=(k*(len(s)//len(k)))+k[:len(s)%len(k)]
+        for i in range(len(s)):
+            if s[i].isalpha():
+                shift=ord(k_rep[i].upper())-ord("A")
+                if f=="e":
+                    output+=chr((ord(s[i])+shift-ord("A"))%26+ord("A"))
+                if f=="d":
+                    output+=chr((ord(s[i])-shift-ord("A"))%26+ord("A"))
+            else: pass
         return output
